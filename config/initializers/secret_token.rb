4f076846dd52de41dc00f30610aea4f6b3063eb4
Jonathan Tang <jdtang13@gmail.com>
2013-10-11 21:31:54 -0400

    make

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cholula::Application.config.secret_key_base = '06bf0d0ed5dd891ef51506fb6d2f912137ff0e523b1b2f751e188f68e6307519373754297b79b8a1963c5a5b5bf3f7688a01b9f550b2945e5862bcae82406fef'
