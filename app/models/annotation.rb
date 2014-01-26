0136437de5a5b51918aaf9d5ac52c81a40f8ea0b
Jonathan Tang <jdtang13@gmail.com>
2014-01-25 19:50:48 -0500

    refinements

--- app/models/annotation.rb
+++ app/models/annotation.rb
@@ -1,4 +1,5 @@
 class Annotation < ActiveRecord::Base
   belongs_to :book
   belongs_to :user
+  
 end
--- db/migrate/20140125200730_create_annotations.rb
+++ db/migrate/20140125200730_create_annotations.rb
@@ -2,7 +2,10 @@ class CreateAnnotations < ActiveRecord::Migration
   def change
     create_table :annotations do |t|
       t.text :body
-      t.integer :location_start
+      
+	  t.integer :location_start
+	  t.integer :location_end
+	  
       t.double :score
       t.references :book, index: true
       t.references :user, index: true
